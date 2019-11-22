7e29b62dcddb498050de1cdbdbecfda258ccff2f
yewbdar <yewbdar.grima@Gmail.com>
2019-11-21 18:09:23 -0600

    initial commit for vue project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+   <Todos v-bind:todos="todos"/>
+  </div>
+</template>
+<script>
+import Todos from './components/Todos';
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data(){
+    return{
+      todos:[{
+        id:1,
+        title:'Todo one',
+        completed:false
+
+      },{
+        id:1,
+        title:'Todo one',
+        completed:true
+        },
+        {
+        id:1,
+        title:'Todo one',
+        completed:false
+        }]
+    }
+  }
+}
+</script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>Todo Item</p>
+    </div>
+</template>
+<script>
+export default {
+    name:'TodoItem',
+
+}
+</script>
+
+<style scoped>
+.todo-item{
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #cccccc dotted
+}
+.is-Complete{
+    text-decoration: line-through
+}
+.del{
+background: #ff0000;
+color:#f4f4f4;
+border:none;
+padding: 5px 9px;
+border-radius: 50%;
+cursor: pointer;
+float: left;
+}
+</style>
